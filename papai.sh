4dc9a315355cdb934997b15f0235ffffc09f5fa5
Matteus Silva <silvamatteus@lsd.ufcg.edu.br>
2017-06-30 14:41:46 -0300

    Create papai.sh

---
+++ papai.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+printf "####################################################################\n"
+printf "#                                                                  #\n"
+printf "#  ::::::::.          ::         ::::::::.          ::         ::  #\n"
+printf "#  ::      ::        ::::        ::      ::        ::::            #\n"
+printf "#  ::      ::       ::  ::       ::      ::       ::  ::       ::  #\n"
+printf "#  ::      ::      ::    ::      ::      ::      ::    ::      ::  #\n"
+printf "#  ::::::::.      ::      ::     ::::::::.      ::      ::     ::  #\n"
+printf "#  ::            ::::::::::::    ::            ::::::::::::    ::  #\n"
+printf "#  ::           ::          ::   ::           ::          ::   ::  #\n"
+printf "#  ::          ::            ::  ::          ::            ::  ::  #\n"
+printf "#  ::         ::              :: ::         ::              :: ::  #\n"
+printf "#                                                                  #\n"
+printf "####################################################################\n"
+
+# >>> Default
+
+printf "\nUpdating and upgrading...\n"
+apt-get update
+apt-get upgrade -y
+
+# >>> Programming languages
+
+printf "\nInstalling tools\n"
+apt-get install git vim python-pip
+printf "set expandtab\nset tabstop=4\nset number\nset autoindent\nsyntax on" > ~/.vimrc
+pip install --upgrade pip
+pip install requests flask simplejson virtualenv
+apt install -y nmap netcat curl
+apt-get install -y oracle-java8-installer
+apt-get install -y oracle-java8-set-default
